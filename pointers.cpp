d7cbb1f7ac4bda09a63ee5d6063256cb736c782c
umar-khan845 <umarmaqbool1877@gmail.com>
2024-03-05 17:14:19 +0500

    updated
    updated

--- pointers.cpp
+++ pointers.cpp
@@ -15,5 +15,13 @@ cout<<"the address of a is "<<&a<<endl;
  // * --> (address at) dereference operator
 cout<<"the value at adress b is "<<*b<<endl;
 
+// pointer to pointer 
+int **c = &b;
+cout<<" the address of b is "<<&b<<endl;
+cout<<" the address of b is "<<c<<endl;
+cout<<" the value at address c is "<<*c<<endl;
+cout<<" the value at address value_at (value_at(c)) is "<<**c<<endl;
+
+
  return 0;
 }
